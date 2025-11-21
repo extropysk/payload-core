cc92ea73259f8abcc9cda3666a91acb0bfb8fecd
martinkova-lucia <l.martinkova23@gmail.com>
2025-11-21 11:39:28 +0100

    fix: spread params (#1)
    * spread params
    
    * remove date override
    
    ---------
    
    Co-authored-by: luciamartinkova <lucia.martinkova1@gmail.com>

--- src/fields/date/index.ts
+++ src/fields/date/index.ts
@@ -40,9 +40,6 @@ export const dateField = ({
       Cell: datePickerCell({ pickerAppearance: admin.date?.pickerAppearance, tz }),
       Field: DatePickerField,
     },
-    date: {
-      pickerAppearance: admin.date?.pickerAppearance,
-    },
   },
   defaultValue,
   hooks,
